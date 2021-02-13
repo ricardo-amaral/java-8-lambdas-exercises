19e2d65fa17531f486cb506792e573694d033ff9
Ricardo Amaral <ricardo.slackware@gmail.com>
2021-02-13 09:51:29 -0300

    [CHAPTER 2] Fixed exercise 2

--- src/main/java/com/insightfullogic/java8/exercises/chapter2/Question2.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter2/Question2.java
@@ -2,10 +2,11 @@ package com.insightfullogic.java8.exercises.chapter2;
 
 import javax.swing.text.DateFormatter;
 import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class Question2 {
 
-    public static final ThreadLocal<DateFormatter> formatter
-            = ThreadLocal.withInitial(() -> new DateFormatter(new SimpleDateFormat("dd-MMM-yyyy")));
+    public static ThreadLocal<DateFormatter> formatter
+            = ThreadLocal.withInitial(() -> new DateFormatter(new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH)));
 
 }
